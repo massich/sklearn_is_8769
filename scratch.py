e3d71d5f369b1565cad0faefc920a9226c3220e2
Joan Massich <sik@visor.udg.edu>
2017-05-31 10:58:11 +0200

    add some stuff to load the tests

--- scratch.py
+++ scratch.py
@@ -1,5 +1,27 @@
 from sklearn import linear_model
-reg = linear_model.LinearRegression()
-X = [[0.0, 0.0], [1.0, 1.0], [2.0, 2.0]]
-y = list( range(3) )
-reg.fit (X, y)
+from sklearn.utils.testing import assert_equal
+from sklearn.linear_model.tests.test_logistic import test_dtype_match, X, Y1, test_multinomial_logistic_regression_string_inputs
+from sklearn.utils.extmath import safe_sparse_dot
+
+import numpy as np
+
+def myDot(a,b):
+    res = np.dot(a,b)
+    return res
+
+
+if __name__ == "__main__":
+    test_multinomial_logistic_regression_string_inputs()
+    test_dtype_match()
+    X_32 = np.array(X*1000000).astype(np.float32)
+    X_64 = np.array(X*1000000).astype(np.float64)
+    w0_32 = np.zeros(X_32.shape[1], dtype=np.float32)
+    w0_64 = np.zeros(X_32.shape[1], dtype=np.float64)
+
+    print(w0_32.shape)
+
+    xx = safe_sparse_dot(X_32, w0_32)
+    xx = safe_sparse_dot(X_32, w0_64)
+    xx = safe_sparse_dot(X_64, w0_32)
+    xx = safe_sparse_dot(X_64, w0_64)
+
