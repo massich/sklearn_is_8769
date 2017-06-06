db9d73d2d0f4fea97102a13b46419119908fc81b
Joan Massich <sik@visor.udg.edu>
2017-06-06 17:15:01 +0200

    Start working with SAGA.
    I'll start by trying to run cd_fast.pyx step by step with the debugger.
    Apparently, cd_fast is called lasso tests

--- scratch.py
+++ scratch.py
@@ -2,6 +2,7 @@ from sklearn import linear_model
 from sklearn.utils.testing import assert_equal
 from sklearn.linear_model.tests.test_logistic import test_dtype_match, X, Y1, test_multinomial_logistic_regression_string_inputs
 from sklearn.utils.extmath import safe_sparse_dot
+from sklearn.covariance.tests import test_graph_lasso
 
 import numpy as np
 
@@ -12,7 +13,7 @@ def myDot(a,b):
 
 if __name__ == "__main__":
     # test_multinomial_logistic_regression_string_inputs()
-    test_dtype_match()
+    # test_dtype_match()
     # X_32 = np.array(X*1000000).astype(np.float32)
     # X_64 = np.array(X*1000000).astype(np.float64)
     # w0_32 = np.zeros(X_32.shape[1], dtype=np.float32)
@@ -25,3 +26,4 @@ if __name__ == "__main__":
     # xx = safe_sparse_dot(X_64, w0_32)
     # xx = safe_sparse_dot(X_64, w0_64)
 
+    test_graph_lasso()
