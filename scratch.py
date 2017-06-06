1d4cefb9516c64b0feff3b89da4bc0a632bde1dc
Joan Massich <sik@visor.udg.edu>
2017-06-06 17:13:58 +0200

    some work

--- scratch.py
+++ scratch.py
@@ -11,17 +11,17 @@ def myDot(a,b):
 
 
 if __name__ == "__main__":
-    test_multinomial_logistic_regression_string_inputs()
+    # test_multinomial_logistic_regression_string_inputs()
     test_dtype_match()
-    X_32 = np.array(X*1000000).astype(np.float32)
-    X_64 = np.array(X*1000000).astype(np.float64)
-    w0_32 = np.zeros(X_32.shape[1], dtype=np.float32)
-    w0_64 = np.zeros(X_32.shape[1], dtype=np.float64)
+    # X_32 = np.array(X*1000000).astype(np.float32)
+    # X_64 = np.array(X*1000000).astype(np.float64)
+    # w0_32 = np.zeros(X_32.shape[1], dtype=np.float32)
+    # w0_64 = np.zeros(X_32.shape[1], dtype=np.float64)
 
-    print(w0_32.shape)
+    # print(w0_32.shape)
 
-    xx = safe_sparse_dot(X_32, w0_32)
-    xx = safe_sparse_dot(X_32, w0_64)
-    xx = safe_sparse_dot(X_64, w0_32)
-    xx = safe_sparse_dot(X_64, w0_64)
+    # xx = safe_sparse_dot(X_32, w0_32)
+    # xx = safe_sparse_dot(X_32, w0_64)
+    # xx = safe_sparse_dot(X_64, w0_32)
+    # xx = safe_sparse_dot(X_64, w0_64)
 
